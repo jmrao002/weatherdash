fcf4cab6e252c751352fd864fd96bd2b8af10f78
Michael Rao <jmichaelrao@gmail.com>
2021-05-24 17:19:35 -0500

    Stylistic changes. Add link to Google Maps.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ let cityFormEl = document.querySelector("#city-search-form");
 let cityInputEl = document.querySelector("#city");
 let weatherContainerEl = document.querySelector("#todays-weather-container");
 let citySearchInputEl = document.querySelector("#searched-city");
+let todaysDateEl = document.querySelector("#todays-date");
 let forecastTextEl = document.querySelector("#forecast-header-text");
 let forecastContainerEl = document.querySelector("#fiveday-container");
 let previousSearchButtonEl = document.querySelector("#search-history-buttons");
@@ -14,22 +15,18 @@ const baseUrl = "https://api.openweathermap.org/data/2.5/";
 // function to get the weather and to throw an error if no data is entered
 function getWeather(e) {
   e.preventDefault();
-  let city = cityInputEl.value.trim();
+  let city = cityInputEl.value.toUpperCase().trim();
   if (!city) {
     alert("Please enter a City");
   } else {
     getCityWeather(city);
     getFiveDay(city);
+    // move city data to front of cityHistoryEl array
     cityHistoryEl.unshift({ city });
     cityInputEl.value = "";
     saveSearch();
     searchHistory(city);
   }
-}
-
-// put search data into local storage
-function saveSearch() {
-  localStorage.setItem("cities", JSON.stringify(cityHistoryEl));
 }
 
 // function to call API to get weather from search term
@@ -43,19 +40,56 @@ function getCityWeather(city) {
   });
 }
 
+// function to get the five day forecast
+function getFiveDay(city) {
+  let apiUrl =
+    baseUrl + "forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      printForecast(data);
+    });
+  });
+}
+
+// put search data into local storage
+function saveSearch() {
+  localStorage.setItem("cities", JSON.stringify(cityHistoryEl));
+}
+
+// function to populate aside with previous search data
+function searchHistory(previousSearch) {
+  searchHistoryEl = document.createElement("button");
+  searchHistoryEl.textContent = previousSearch;
+  searchHistoryEl.classList = "d-flex w-100 btn-dark rounded border p-2";
+  searchHistoryEl.setAttribute("data-city", previousSearch);
+  searchHistoryEl.setAttribute("type", "submit");
+  previousSearchButtonEl.prepend(searchHistoryEl);
+}
+
 // print weather data to screen
-function printWeather(weather, searchCity) {
+function printWeather(weather) {
   // unhide page container
   let pageContainerEl = document.querySelector("#page-container");
   pageContainerEl.classList.remove("d-none");
   // clear old content when searching for a new city
   weatherContainerEl.textContent = "";
-  citySearchInputEl.textContent = searchCity;
-  // add date el
+  citySearchInputEl.setAttribute(
+    "href",
+    "https://google.com/maps/place/" +
+      weather.name +
+      "/@" +
+      weather.coord.lat +
+      "," +
+      weather.coord.lon +
+      ",10z"
+  );
+  citySearchInputEl.textContent = weather.name + ", " + weather.sys.country;
+  // add/remove date el
+  todaysDateEl.textContent = "";
   let currentDate = document.createElement("span");
   currentDate.textContent =
     "   -   " + moment(weather.dt.value).format("dddd MMM D");
-  citySearchInputEl.appendChild(currentDate);
+  todaysDateEl.appendChild(currentDate);
   // add image el for icon
   let wIcon = document.createElement("img");
   let icon = weather.weather[0].icon;
@@ -64,37 +98,30 @@ function printWeather(weather, searchCity) {
     "https://openweathermap.org/img/wn/" + icon + ".png"
   );
   // append icon
-  citySearchInputEl.appendChild(wIcon);
+  todaysDateEl.appendChild(wIcon);
   console.log(weather);
-
   // create el for temp data
   let tempEl = document.createElement("span");
-  tempEl.textContent =
-    "Temperature: " + Math.round(weather.main.temp) + "°F";
+  tempEl.textContent = "Temperature: " + Math.round(weather.main.temp) + "°F";
   tempEl.classList = "list-group-item";
-
   // create el for feels like temp
   let feelsLikeEl = document.createElement("span");
   feelsLikeEl.textContent =
     "Feels Like: " + Math.round(weather.main.feels_like) + "°F";
   feelsLikeEl.classList = "list-group-item";
-
   // create el for humidity data
   let humidityEl = document.createElement("span");
   humidityEl.textContent = "Humidity: " + weather.main.humidity + "%";
   humidityEl.classList = "list-group-item";
-
   // create el for wind data
   let windSpeedEl = document.createElement("span");
   windSpeedEl.textContent = "Wind: " + Math.round(weather.wind.speed) + " MPH";
   windSpeedEl.classList = "list-group-item";
-
   //append to weather container
   weatherContainerEl.appendChild(tempEl);
   weatherContainerEl.appendChild(feelsLikeEl);
   weatherContainerEl.appendChild(humidityEl);
   weatherContainerEl.appendChild(windSpeedEl);
-
   // define latitute and longitude for following API call
   let lat = weather.coord.lat;
   let lon = weather.coord.lon;
@@ -116,10 +143,8 @@ function printUvIndex(index) {
   let uvIndexEl = document.createElement("div");
   uvIndexEl.textContent = "UV Index: ";
   uvIndexEl.classList = "list-group-item";
-
   uvIndexValue = document.createElement("span");
   uvIndexValue.textContent = index.value;
-
   if (index.value <= 2) {
     uvIndexValue.classList = "rounded favorable";
   } else if (index.value > 2 && index.value <= 8) {
@@ -133,29 +158,16 @@ function printUvIndex(index) {
   weatherContainerEl.appendChild(uvIndexEl);
 }
 
-// function to get the five dat forecast
-function getFiveDay(city) {
-  let apiUrl =
-    baseUrl + "forecast?q=" + city + "&units=imperial&appid=" + apiKey;
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      printForecast(data);
-    });
-  });
-}
-
 // function to print 5 day forecast
 function printForecast(weather) {
   forecastContainerEl.textContent = "";
   forecastTextEl.textContent = "5-Day Forecast";
-
   let forecast = weather.list;
+  // loop to print weather for next 5 days
   for (let i = 5; i < forecast.length; i = i + 8) {
     let dailyForecast = forecast[i];
-
     let forecastEl = document.createElement("div");
-    forecastEl.classList = "card bg-info text-light m-2";
-
+    forecastEl.classList = "card bg-info text-light m-2 p-4";
     //create date el
     let forecastDate = document.createElement("h4");
     forecastDate.textContent = moment
@@ -163,7 +175,6 @@ function printForecast(weather) {
       .format("ddd MMM D");
     forecastDate.classList = "card-header text-center";
     forecastEl.appendChild(forecastDate);
-
     //create an image el
     let wIcon = document.createElement("img");
     wIcon.classList = "card-body text-center";
@@ -172,44 +183,29 @@ function printForecast(weather) {
       "src",
       "https://openweathermap.org/img/wn/" + icon + ".png"
     );
-
     //append icon
     forecastEl.appendChild(wIcon);
-
     //create and append temp span
     let forecastTempEl = document.createElement("span");
     forecastTempEl.classList = "card-body";
     forecastTempEl.textContent =
       "Temp: " + Math.round(dailyForecast.main.temp) + " °F";
     forecastEl.appendChild(forecastTempEl);
-
     //create and append wind span
     let forecastWindEl = document.createElement("span");
     forecastWindEl.classList = "card-body";
     forecastWindEl.textContent =
       "Wind: " + Math.round(dailyForecast.wind.speed) + " MPH";
     forecastEl.appendChild(forecastWindEl);
-
     // create and append humidity span
     let forecastHumidityEl = document.createElement("span");
     forecastHumidityEl.classList = "card-body";
     forecastHumidityEl.textContent =
       "Humidity: " + dailyForecast.main.humidity + "%";
     forecastEl.appendChild(forecastHumidityEl);
-
     //append to five day container
     forecastContainerEl.appendChild(forecastEl);
   }
-}
-
-// function to populate aside with previous search data
-function searchHistory(previousSearch) {
-  searchHistoryEl = document.createElement("button");
-  searchHistoryEl.textContent = previousSearch;
-  searchHistoryEl.classList = "d-flex w-100 btn-dark rounded border p-2";
-  searchHistoryEl.setAttribute("data-city", previousSearch);
-  searchHistoryEl.setAttribute("type", "submit");
-  previousSearchButtonEl.prepend(searchHistoryEl);
 }
 
 // function to run search again from previous
